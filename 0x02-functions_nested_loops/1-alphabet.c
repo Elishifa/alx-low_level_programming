03704451f47cbf7816d84096731355d0432dde93
ELISHIFA <elishwanga@gmail.com>
2022-12-14 05:12:47 -0800

    Alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
 /**
  * print_alphabet - Prints all alphabets in lowercase
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++;)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
