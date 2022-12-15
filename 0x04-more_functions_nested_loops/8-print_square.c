48b0e0f0d056a9932de9a456048c1a997f8f23ba
ELISHIFA <elishwanga@gmail.com>
2022-12-15 09:32:12 -0800

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: square size
+ * Return: always 0
+ */
+
+void print_square(int size)
+{
+	int m, n;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (m = 0; m < size; m++)
+	{
+		for (n = 0; n < (size); n++)
+		{
+			_putchar('0');
+		}
+		_putchar('\n');
+	}
+}
