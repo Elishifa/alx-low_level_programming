68be87b8aa774b96e0468bc4a11aebe513dba9bb
ELISHIFA <elishwanga@gmail.com>
2022-12-15 08:30:53 -0800

    except 2 and 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,16 @@
 
 /**
  * print_numbers - print 0 to 9
- * Return: Always 0
+ * @c: number being printed
+ * Return: void
  */
 void print_numbers(void)
 {
 	char c;
 
-	for (c >= '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
