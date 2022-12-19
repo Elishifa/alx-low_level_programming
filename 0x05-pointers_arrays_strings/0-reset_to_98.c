35fecf8cd1ef0a9eb5efe673c8afd68f1d41aef1
ELISHIFA <elishwanga@gmail.com>
2022-12-19 08:49:15 -0800

    Parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that updates the value it points to to 98
+ * @n: parameter integer
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
