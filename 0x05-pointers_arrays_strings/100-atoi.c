b7f8e0c1e454ec04b2f34c55d93e637c335904fe
ELISHIFA <elishwanga@gmail.com>
2022-12-19 16:08:49 -0800

    generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: character
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	
