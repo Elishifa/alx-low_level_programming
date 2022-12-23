9ff11948abf21eee24c2f061d20ebd623844dbc9
ELISHIFA <elishwanga@gmail.com>
2022-12-23 13:21:22 -0800

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+rinclude "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: capitalised string
+ * Return: string address
+ */
+
+char *cap_string(char *str)
+{
+	int m = 0, p;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(str + m))
+	{
+		if (*(str + m) >= 'a' && *(str + m) <= 'z')
+		{
+			if (m == 0)
+				*(str + m) -= 'a' - 'A';
+			else
+			{
+				for (p = 0; p <= 12; p++)
+				{
+					if (a[j] == *(str + m - 1))
+						*(str + m) -= 'a' - 'A';
+				}
+			}
+		}
+		m++;
+	}
+	return (str);
+}
