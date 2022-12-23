2101717abaec6707c936b9d58a45f47c9627c5dc
ELISHIFA <elishwanga@gmail.com>
2022-12-23 13:46:54 -0800

    encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: encoded string
+ * Return: string
+ */
+
+char *leet(char *str)
+{
+	int m, n;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (m = 0; *(str + m); m++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			if (s1[m] == str[n])
+				str[m] = s2[n];
+		}
+	}
+	return (str);
+}
