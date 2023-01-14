668e31f05d1309370ced9701adc618178d7a0936
ELISHIFA <elishwanga@gmail.com>
2023-01-14 06:20:09 -0800

    multiplication of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - multiplication
+ * @argc: args count
+ * @argv: containing command line args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i * j);
+	return (0);
+}
