8a6ed496822a6ac54f246fb2ab3b9aeee52786b8
ELISHIFA <elishwanga@gmail.com>
2023-01-14 06:49:46 -0800

    adding positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+iinclude <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adding postive numbers
+ * @argc: args count
+ * @argv: contains command line args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (a = 1; argv[a]; a++)
+	{
+		n = strtol(argv[a], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += b;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
