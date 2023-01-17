2b2d0aa31dde9bc810ea376e064ad751e7b65cb7
ELISHIFA <elishwanga@gmail.com>
2023-01-17 07:04:56 -0800

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from\n
+ * Return: always o
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+		return (0);
+}
