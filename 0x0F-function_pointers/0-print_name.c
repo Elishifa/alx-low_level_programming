41fe9d64ae09dc3170c4fd8a43819513319e335f
ELISHIFA <elishwanga@gmail.com>
2023-01-19 14:24:22 -0800

    say my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints name
+ * @name: name to be printed
+ * @f: funct pointer
+ * Return: always 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+	return (0);
+}
